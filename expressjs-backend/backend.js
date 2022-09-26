0e564d47c792840a1a0ca7b3398ae5724193d2ef
Peter Marsh <peter_marsh@hotmail.com>
2022-09-26 13:59:55 -0700

    delelte implememnted

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -55,6 +55,46 @@ const findUserByName = (name) => {
     return users['users_list'].filter( (user) => user['name'] === name); 
 }
 
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.send(result);
+    }
+});
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
+app.delete('/users', (req, res) => {
+    const id = req.query.id;
+    if (id === undefined) {
+        res.status(500).send("No ID specified")
+    } else {
+        console.log(users.users_list);
+        index = users.users_list.findIndex(user => user.id == id);
+        console.log(index);
+        users.users_list.splice(index, 1);
+        console.log(users.users_list);
+    }
+    res.status(200).end();
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });      
